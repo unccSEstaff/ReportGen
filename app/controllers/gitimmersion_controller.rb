9c3a80805d8c906fa5ee9e3cb788a4dd1a734b7f
jhasson84 <jhasson@uncc.edu>
2015-04-01 23:27:47 -0300

    actual working view

--- app/controllers/gitimmersion_controller.rb
+++ app/controllers/gitimmersion_controller.rb
@@ -6,17 +6,12 @@ class GitimmersionController < ApplicationController
   end
 
   def process_students
+	debugger
 	int score=0
-    csv_array = []
-    csv_array<< ['Name', 'NinerNet ID', 'GitHub Username', 'Repository Name','Number of Commits', 'Number of Branches', 'Score']
+    client = Octokit::Client.new login: params[:username], password: params[:password]
   end
 
-  client = Octokit::Client.new login: params[:username], password: params[:password]
 
-  Student.order(:name).all.each do |student|
-	
-  if (Octokit.repository? ENV['student.github_username']+'/gitimmersion') 
-  then 
-	score=score+5
-  end
+  
+
 end
